457c8cef60d81affec5bd9f1cfc081b679f484c0
TheProCoder18 <91504105+TheProCoder18@users.noreply.github.com>
2022-06-09 11:47:08 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Intialized!');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + rightWristX +" noseY = " + noseY);
+
+        
+
+        console.log("leftWristX = " + leftWristX + " rightWristX = "+ rightWristX + "diffrence = " + difference);
+
+    }
+}
+
+function draw() {
+    background('#6C91C2');
+
+    document.getElementById("font_size").innerHTML = "Font size of the text will be  = " + difference + "px";
+    textSize(diffrence);
+    fill('#FFE787');
+    
+    text('Peter', 50, 400);
+}
+
